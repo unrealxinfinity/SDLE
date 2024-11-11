f20840c726e784c3e9631087bd4197f195ac272d
unrealxinfinity <fuhaochang664961423@hotmail.com>
2024-11-11 00:05:39 +0000

    added some docs

--- src/crdt/DeltaORMap.ts
+++ src/crdt/DeltaORMap.ts
@@ -11,7 +11,11 @@ class DeltaORMap {
     this.causalContext = new Map<string,[number,Map<string,number>]>;
     this.causalContext.set(this.id,[0,new Map<string,number>()]);
   }
-  //adds a new delta value to the context (delta positive);
+  /**
+   * adds a new delta value to the context (delta positive);
+   * @param {string} item - the item to be added
+   * @param {number} delta - the quantity of the item to be added
+   */
   add(item:string, delta:number = 1){
     /*if(!this.delta.has(item)){
       this.delta.set(item,new DeltaContext(item,this.causalContext.get(this.id)));
@@ -24,7 +28,13 @@ class DeltaORMap {
       this.causalContext.get(this.id)[1].set(item,this.causalContext.get(this.id)[1].get(item)+delta);
     }
   }
-  //removes an item and updates the delta context by adding a removal delta (delta negative);
+  /**
+   * removes an item and updates the delta context by adding a removal delta (delta negative);
+   * @param {string} item - the item to be removed
+   * @param {number} delta - the quantity of the item to be removed
+   * @throws {Error} - if the item is not found for removal
+   * @throws {Error} - if the quantity of the item to be removed is greater than the quantity of the item
+  */
   remove(item:string,delta:number = 1){
     /*if(!this.delta.has(item)){
       throw new Error("Item not found for removal");
@@ -37,11 +47,14 @@ class DeltaORMap {
       throw new Error("Can't remove more items than exist");
     }
     else{
-      this.causalContext.get(this.id)[0]++;
       this.causalContext.get(this.id)[1].set(item,this.delta.get(item)-delta);
     }
   }
-  // Method to join two DeltaORMap instances
+  /**
+   * Method to join two DeltaORMap instances
+   * @param {DeltaORMap} other - the other DeltaORMap instance to be joined
+   */
+
   join(other: DeltaORMap) {
     this.sortCausalContextByKey();
     other.sortCausalContextByKey();
@@ -73,11 +86,15 @@ class DeltaORMap {
 
     this.joinDelta(other);
   }
-  insertContext(listID:string,otherCausal:Map<string,[number,Map<string,number>]>){
+  private insertContext(listID:string,otherCausal:Map<string,[number,Map<string,number>]>){
     this.causalContext.set(listID,otherCausal.get(listID));
   }
-  // Checks of context is in this context (comparing indexes the higher the more recent);
-  contextInList(listID:string,other:DeltaORMap){
+  /**
+   * Checks of context is in this context (comparing indexes the higher the more recent);
+   * @param {string} listID - the list id to be checked
+   * @param {DeltaORMap} other - the other DeltaORMap instance to be compared
+   */ 
+  private contextInList(listID:string,other:DeltaORMap){
     if(!this.causalContext.has(listID)){
       return false;
     }
@@ -86,7 +103,11 @@ class DeltaORMap {
     let [otherContext,otherDeltaMap] = other.getCausal().get(listID);
     return thisContext>=otherContext;
   }
-  deleteContext(listID:string){
+  /**
+   * Deletes the context associated with listID from this instance's causal context
+   * @param {string} listID - the list id to be deleted
+   */
+  private deleteContext(listID:string){
     if(!this.causalContext.has(listID)){
       throw new Error("List not found for deletion");
     }
@@ -94,7 +115,10 @@ class DeltaORMap {
       this.causalContext.delete(listID);
     }
   }
-  sortCausalContextByKey() {
+  /**
+   * Sorts the causal context by key
+   */
+  private sortCausalContextByKey() {
     const sortedEntries = Array.from(this.causalContext.entries()).sort(([keyA], [keyB]) => {
       if (keyA < keyB) return -1;
       if (keyA > keyB) return 1;
@@ -102,7 +126,11 @@ class DeltaORMap {
     });
     this.causalContext = new Map(sortedEntries);
   }
-  joinDelta (other:DeltaORMap){
+  /**
+   * Aux func to join two DeltaORMap instances
+   * @param other - the other DeltaORMap instance to be joined
+   */
+  private joinDelta (other:DeltaORMap){
     this.sortCausalContextByKey();
     other.sortCausalContextByKey();
     const otherCausal = other.getCausal();
@@ -142,16 +170,28 @@ class DeltaORMap {
     }  while (!(thisIter===thisEntries.length) || !(otherIter===otherEntries.length));
       this.products = this.mergeDeltaProductList(this.causalContext.get(this.id),this.causalContext.get(other.getName()));
   }
-  
-  changeDeltaMapAt(listID:string,deltaMap:[number,Map<string,number>]){
+  /**
+   * Changes the delta map at the listID with the new deltaMap and the version associated;
+   * @param {string} listID - the list id to be changed
+   * @param {[number,Map<string,number>]} deltaMap - the new delta map to be set
+   */
+  private changeDeltaMapAt(listID:string,deltaMap:[number,Map<string,number>]){
     let [otherVersion,otherDeltaMap] = deltaMap;
     this.causalContext.set(listID,[otherVersion,otherDeltaMap]);
   }
-  changeDeltaMapWithInc(listID:string,deltaMap:[number,Map<string,number>]){
+  
+  private changeDeltaMapWithInc(listID:string,deltaMap:[number,Map<string,number>]){
     let [otherVersion,otherDeltaMap] = deltaMap;
     this.causalContext.set(listID,[otherVersion+1,otherDeltaMap]);
   }
-  mergeDeltaProductList(context:[number,Map<string,number>],context2:[number,Map<string,number>]){
+  /**
+   * Merges the delta product list of two contexts and returns the merged list;
+   * @param {[number,Map<string,number>]} context - the first context to be merged
+   * @param {[number,Map<string,number>]} context2 - the second context to be merged
+   * @returns {Map<string,number>} - the merged product list
+   * @throws {Error} - if the quantity of the item to be removed is greater than the quantity of the item
+   */
+  private mergeDeltaProductList(context:[number,Map<string,number>],context2:[number,Map<string,number>]){
   let productList = new Map<string,number>();
   let deltaMap = context[1];
   let stubDeltaMap = this.copyMap(deltaMap);
@@ -177,14 +217,27 @@ class DeltaORMap {
       }
       else{
         const productQuantity = this.products.get(product);
+        if(productQuantity+value<0){
+          throw new Error("Can't remove more items than exist");
+        }
         productList.set(product,productQuantity+value);
       }
     }
     return productList;
   }   
-  copyMap(originalMap: Map<string, number>): Map<string, number> {
+  /**
+   *  Copies a map
+   * @param {Map<string,number>} originalMap 
+   * @returns {Map<string,number>} - A new copied map
+   */
+  private copyMap(originalMap: Map<string, number>): Map<string, number> {
     return new Map(originalMap);
   }
+  /**
+   * Reads the quantity of a product
+   * @param {string} product 
+   * @returns {number} - the quantity of the product
+   */
   read(product:string){
     return this.products.get(product);
   }
